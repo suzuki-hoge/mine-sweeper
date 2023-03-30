e8229e2b2aa8ac9708d1d411489f23b5af03f56f
suzuki-hoge <user.ryo@gmail.com>
2023-03-30 23:40:09 +0900

    sweep in a chain.

--- mine-sweeper/src-tauri/src/game.rs
+++ mine-sweeper/src-tauri/src/game.rs
@@ -12,31 +12,55 @@ impl Game {
     // 盤面を初期化する
     pub fn init(w: usize, h: usize) -> Self {
         let dots = vec![
-            vec![Wall, Wall, Wall, Wall, Wall],
-            vec![Wall, Unexplored, Unexplored, Unexplored, Wall],
-            vec![Wall, Unexplored, Unexplored, Unexplored, Wall],
-            vec![Wall, Mine, Unexplored, FlaggedMine, Wall],
-            vec![Wall, Wall, Wall, Wall, Wall],
+            vec![Unexplored, Unexplored, Unexplored],
+            vec![Unexplored, Unexplored, Unexplored],
+            vec![Mine, Unexplored, FlaggedMine],
         ];
         Self { w, h, dots }
     }
 
     // 選択した箇所を更新し、選択結果を返す
     pub fn sweep(&mut self, x: usize, y: usize) -> Swept {
-        match &self.dots[y + 1][x + 1] {
+        match &self.dots[y][x] {
             &Mine | &FlaggedMine => Bomb,
             &Number(_) | &Wall => Stay,
             &Unexplored | &FlaggedSafe => {
                 let mut mines = 0;
-                for yd in y..=y + 2 {
-                    for xd in x..=x + 2 {
-                        let dot = &self.dots[y + yd][x + xd];
+                for yd in 0..3 {
+                    for xd in 0..3 {
+                        if y + yd == 0
+                            || x + xd == 0
+                            || y + yd == self.h + 1
+                            || x + xd == self.w + 1
+                        {
+                            continue;
+                        }
+                        let dot = &self.dots[y + yd - 1][x + xd - 1];
                         if dot == &Mine || dot == &FlaggedMine {
                             mines += 1;
                         }
                     }
                 }
-                self.dots[y + 1][x + 1] = Number(mines);
+                self.dots[y][x] = Number(mines);
+
+                // 0 の場合は連鎖させる
+                if mines == 0 {
+                    for yd in 0..3 {
+                        for xd in 0..3 {
+                            if y + yd == 0
+                                || x + xd == 0
+                                || y + yd == self.h + 1
+                                || x + xd == self.w + 1
+                            {
+                                continue;
+                            }
+                            let dot = &self.dots[y + yd - 1][x + xd - 1];
+                            if dot == &Unexplored {
+                                self.sweep(x + xd - 1, y + yd - 1);
+                            }
+                        }
+                    }
+                }
                 Safe
             }
         }
@@ -78,23 +102,35 @@ pub enum Swept {
 
 #[cfg(test)]
 mod tests {
-    use crate::game::Dot::{Number, Unexplored};
+    use crate::game::Dot::{FlaggedMine, Mine, Number, Unexplored};
     use crate::game::Game;
     use crate::game::Swept::{Bomb, Safe, Stay};
 
     #[test]
     fn sweep() {
         let mut game = Game::init(3, 3);
-        assert_eq!(&Unexplored, &game.dots[0 + 1][0 + 1]);
+        assert_eq!(&Unexplored, &game.dots[0][0]);
 
         // 未探索が 0 に更新されている
         let swept = game.sweep(0, 0);
-        assert_eq!(&Number(0), &game.dots[0 + 1][0 + 1]);
+        assert_eq!(&Number(0), &game.dots[0][0]);
         assert_eq!(Safe, swept);
 
-        // 探索済みならそのままである
+        // 0 のマスの周囲は連鎖解放されている
+        assert_eq!(&Number(0), &game.dots[0][1]);
+        assert_eq!(&Number(0), &game.dots[0][2]);
+        assert_eq!(&Number(1), &game.dots[1][0]);
+        assert_eq!(&Number(2), &game.dots[1][1]);
+        assert_eq!(&Number(1), &game.dots[1][2]);
+
+        // 地雷と範囲外は据え置き
+        assert_eq!(&Mine, &game.dots[2][0]);
+        assert_eq!(&Unexplored, &game.dots[2][1]);
+        assert_eq!(&FlaggedMine, &game.dots[2][2]);
+
+        // 探索済みをクリックしても何も起きない
         let swept = game.sweep(0, 0);
-        assert_eq!(&Number(0), &game.dots[0 + 1][0 + 1]);
+        assert_eq!(&Number(0), &game.dots[0][0]);
         assert_eq!(Stay, swept);
 
         // 地雷を踏んだら終了
